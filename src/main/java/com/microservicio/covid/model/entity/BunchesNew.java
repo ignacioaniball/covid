4cc659cba25dbd6178d7936ef0c47d983875a287
ignacioaniball <ignacioaniball@gmail.com>
2020-10-31 21:42:36 -0300

    CB-01 Add Entity classes BunchesNews and NewsController controller.

---
+++ src/main/java/com/microservicio/covid/model/entity/BunchesNew.java
@@ -0,0 +1,17 @@
+package com.microservicio.covid.model.entity;
+
+import java.util.List;
+
+public class BunchesNew {
+
+    private List<News> news;
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+
+}
